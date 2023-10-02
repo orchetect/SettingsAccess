f37dd29b7a1224fba5f9caa7891ddc55ac368551
Steffan Andrews <stef@orchetect.com>
2023-10-01 20:50:35 -0700

    Refactored to use background instead of ZStack

--- Sources/SettingsAccess/OpenSettingsInjectionView.swift
+++ Sources/SettingsAccess/OpenSettingsInjectionView.swift
@@ -80,7 +80,7 @@ internal struct OpenSettingsInjectionView<Content: View>: View {
         if #available(macOS 14, *) {
             // make a hidden settings link so we can hijack its button action
             content
-                .overlay {
+                .background {
                     #if swift(>=5.9) // prevents compile error in Xcode 14 because SettingsLink is not in its macOS SDK
                     SettingsLink {
                         // Text(verbatim: "")
