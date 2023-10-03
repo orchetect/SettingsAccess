4cd6d0e477a4dba67739fa2f30038c8ad87cae52
Steffan Andrews <stef@orchetect.com>
2023-10-03 14:52:25 -0700

    Updated inline docs

--- Sources/SettingsAccess/OpenSettingsAccessAction.swift
+++ Sources/SettingsAccess/OpenSettingsAccessAction.swift
@@ -11,6 +11,40 @@ import SwiftUI
 
 // Note: we shouldn't name this `OpenSettingsAction` because that's the name of Apple's private action
 /// An action that opens the app's Settings scene.
+///
+/// Use the ``SwiftUI/EnvironmentValues/openSettings`` environment value to get the instance of this structure for a given `Environment`.
+/// Then call the instance to open the Settings scene.
+/// You call the instance directly because it defines a ``callAsFunction()`` method that Swift calls when you call the instance.
+///
+/// In order for `openSettings` to operate, the `openSettingsAccess` view modifier must be applied to an ancestor of the view hierarchy.
+///
+/// Attach the `openSettingsAccess` view modifier to the base view whose subviews needs access to the `openSettings` method.
+///
+/// ```swift
+/// @main
+/// struct MyApp: App {
+///     var body: some Scene {
+///         WindowGroup {
+///             ContentView()
+///                 .openSettingsAccess()
+///         }
+///
+///         Settings { SettingsView() }
+///     }
+/// }
+/// ```
+///
+/// In any subview where needed, add the environment method declaration. Then the `Settings` scene may be opened programmatically by calling this method.
+///
+/// ```swift
+/// struct ContentView: View {
+///     @Environment(\.openSettings) private var openSettings
+///
+///     var body: some View {
+///         Button("Open Settings") { openSettings() }
+///     }
+/// }
+/// ```
 public class OpenSettingsAccessAction: ObservableObject {
     // Closure to run when `openSettings()` is called.
     // Default to legacy Settings/Preferences window call.
