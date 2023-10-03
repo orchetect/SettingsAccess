f28e85c1586324158deb33b0c134fab0daa9dd28
Steffan Andrews <stef@orchetect.com>
2023-10-03 14:39:58 -0700

    File organization

--- Sources/SettingsAccess/Environment.swift
+++ Sources/SettingsAccess/Environment.swift
@@ -29,45 +29,4 @@ extension EnvironmentValues {
     }
 }
 
-// Note: we shouldn't name this `OpenSettingsAction` because that's the name of Apple's private action
-/// An action that opens the app's Settings scene.
-public class OpenSettingsAccessAction: ObservableObject {
-    // Closure to run when `openSettings()` is called.
-    // Default to legacy Settings/Preferences window call.
-    // This closure will be replaced with the new SettingsLink trigger later.
-    @Published
-    var closure: () -> Void = {
-        openSettingsLegacyOS()
-    }
-    
-    private(set) var closureBinding: Binding<() -> Void> = .constant({ })
-    
-    // Set up a binding that allows us to update the closure property with a new closure later.
-    internal init() {
-        closureBinding = Binding(
-            get: { [weak self] in
-                self?.closure ?? { }
-            }, set: {  [weak self] newValue in
-                self?.closure = newValue
-            }
-        )
-    }
-    
-    /// Don’t call this method directly. SwiftUI calls it for you when you call the `OpenSettingsAccessAction` instance that you get from the Environment:
-    ///
-    /// ```swift
-    /// struct ContentView: View {
-    ///     @Environment(\.openSettings) private var openSettings
-    ///
-    ///     var body: some View {
-    ///         Button("Open Settings") {
-    ///             openSettings() // Implicitly calls openSettings.callAsFunction()
-    ///         }
-    ///     }
-    /// }
-    public func callAsFunction() {
-        closure()
-    }
-}
-
 #endif
